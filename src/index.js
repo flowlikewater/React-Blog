26e3f99922195934bb9cfb839fca40c9206877f6
Kevin <cho.kevin@hotmail.com>
2017-09-10 19:10:14 +0800

    react router works with dummy components in index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+// BrowserRouter interacts with the history
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
+
+// <Route path="/hello" component={Hello} /> means:
+// if the url goes to the path="", then display this component
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        HEADER
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
